f5713d8ab409af75af86a0aa4de1dce65db1b984
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-12-04 20:27:19 -0800

    add ModelController

--- src/main/java/com/vcabading/displaydate/DisplaydateApplication.java
+++ src/main/java/com/vcabading/displaydate/DisplaydateApplication.java
@@ -3,6 +3,10 @@ package com.vcabading.displaydate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/////////////////////////////////////////////////////////////////
+//	DISPLAY DATE APPLICATION
+/////////////////////////////////////////////////////////////////
+
 @SpringBootApplication
 public class DisplaydateApplication {
 
---
+++ src/main/java/com/vcabading/displaydate/HomeController.java
@@ -0,0 +1,17 @@
+package com.vcabading.displaydate;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/////////////////////////////////////////////////////////////////
+//HOME CONTROLLER
+/////////////////////////////////////////////////////////////////
+
+@Controller
+public class HomeController {
+@GetMapping("/")
+public String index(Model model) {
+	return "index.jsp";
+}
+}
